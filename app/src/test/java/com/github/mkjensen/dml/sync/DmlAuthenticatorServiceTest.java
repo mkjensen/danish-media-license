033c08569a43d99734286ae7fc8ffd1590e215d2
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2016-03-12 15:16:17 +0100

    Add tests for DmlAuthenticatorService

---
+++ app/src/test/java/com/github/mkjensen/dml/sync/DmlAuthenticatorServiceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Martin Kamp Jensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mkjensen.dml.sync;
+
+import static org.junit.Assert.assertEquals;
+
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import com.github.mkjensen.dml.RobolectricTest;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link DmlAuthenticatorService}.
+ */
+public class DmlAuthenticatorServiceTest extends RobolectricTest {
+
+  @Test
+  public void onBind_givenOnCreateHasBeenCalled_returnsIAccountAuthenticator()
+      throws RemoteException {
+
+    // Given
+    DmlAuthenticatorService authenticatorService = new DmlAuthenticatorService();
+    authenticatorService.onCreate();
+
+    // When
+    IBinder binder = authenticatorService.onBind(null);
+
+    // When
+    assertEquals("android.accounts.IAccountAuthenticator", binder.getInterfaceDescriptor());
+  }
+}
