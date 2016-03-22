aac5057e574aae317363ab6270433cf01483fa2d
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2016-03-22 08:24:37 +0100

    Use OkHttp HTTP client with ExoPlayer and Glide (in addition to Retrofit)

--- app/src/main/java/com/github/mkjensen/dml/exoplayer/HlsRendererBuilder.java
+++ app/src/main/java/com/github/mkjensen/dml/exoplayer/HlsRendererBuilder.java
@@ -39,6 +39,8 @@ import com.google.android.exoplayer.upstream.DataSource;
 import com.google.android.exoplayer.upstream.DefaultAllocator;
 import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer.upstream.DefaultUriDataSource;
+import com.google.android.exoplayer.upstream.TransferListener;
+import com.google.android.exoplayer.upstream.UriDataSource;
 import com.google.android.exoplayer.util.ManifestFetcher;
 import com.google.android.exoplayer.util.ManifestFetcher.ManifestCallback;
 
@@ -49,11 +51,15 @@ import android.os.Handler;
 
 import com.github.mkjensen.dml.exoplayer.DemoPlayer.RendererBuilder;
 
+import okhttp3.OkHttpClient;
+
 import java.io.IOException;
 import java.util.List;
 
 /**
  * A {@link RendererBuilder} for HLS.
+ *
+ * @see <a href="https://goo.gl/QaNCjx">ExoPlayer HlsRendererBuilder</a>
  */
 public class HlsRendererBuilder implements RendererBuilder {
 
@@ -61,12 +67,17 @@ public class HlsRendererBuilder implements RendererBuilder {
   private static final int MAIN_BUFFER_SEGMENTS = 256;
   private static final int TEXT_BUFFER_SEGMENTS = 2;
 
+  private static OkHttpClient httpClient;
+
   private final Context context;
   private final String userAgent;
   private final String url;
 
   private AsyncRendererBuilder currentAsyncBuilder;
 
+  /**
+   * Creates a new renderer builder for content served using HLS.
+   */
   public HlsRendererBuilder(Context context, String userAgent, String url) {
     this.context = context;
     this.userAgent = userAgent;
@@ -103,8 +114,7 @@ public class HlsRendererBuilder implements RendererBuilder {
       this.url = url;
       this.player = player;
       HlsPlaylistParser parser = new HlsPlaylistParser();
-      playlistFetcher = new ManifestFetcher<>(url, new DefaultUriDataSource(context, userAgent),
-          parser);
+      playlistFetcher = new ManifestFetcher<>(url, createDataSource(context, userAgent), parser);
     }
 
     public void init() {
@@ -116,12 +126,12 @@ public class HlsRendererBuilder implements RendererBuilder {
     }
 
     @Override
-    public void onSingleManifestError(IOException e) {
+    public void onSingleManifestError(IOException ex) {
       if (canceled) {
         return;
       }
 
-      player.onRenderersError(e);
+      player.onRenderersError(ex);
     }
 
     @Override
@@ -136,7 +146,7 @@ public class HlsRendererBuilder implements RendererBuilder {
       PtsTimestampAdjusterProvider timestampAdjusterProvider = new PtsTimestampAdjusterProvider();
 
       // Build the video/audio/metadata renderers.
-      DataSource dataSource = new DefaultUriDataSource(context, bandwidthMeter, userAgent);
+      DataSource dataSource = createDataSource(context, userAgent, bandwidthMeter);
       HlsChunkSource chunkSource = new HlsChunkSource(true /* isMaster */, dataSource, url,
           manifest, DefaultHlsTrackSelector.newDefaultInstance(context), bandwidthMeter,
           timestampAdjusterProvider, HlsChunkSource.ADAPTIVE_MODE_SPLICE);
@@ -145,10 +155,10 @@ public class HlsRendererBuilder implements RendererBuilder {
       MediaCodecVideoTrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(context,
           sampleSource, MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT,
           5000, mainHandler, player, 50);
-      MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource,
-          MediaCodecSelector.DEFAULT, null, true, player.getMainHandler(), player,
+      final MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(
+          sampleSource, MediaCodecSelector.DEFAULT, null, true, player.getMainHandler(), player,
           AudioCapabilities.getCapabilities(context), AudioManager.STREAM_MUSIC);
-      MetadataTrackRenderer<List<Id3Frame>> id3Renderer = new MetadataTrackRenderer<>(
+      final MetadataTrackRenderer<List<Id3Frame>> id3Renderer = new MetadataTrackRenderer<>(
           sampleSource, new Id3Parser(), player, mainHandler.getLooper());
 
       // Build the text renderer, preferring Webvtt where available.
@@ -158,7 +168,7 @@ public class HlsRendererBuilder implements RendererBuilder {
       }
       TrackRenderer textRenderer;
       if (preferWebvtt) {
-        DataSource textDataSource = new DefaultUriDataSource(context, bandwidthMeter, userAgent);
+        DataSource textDataSource = createDataSource(context, userAgent, bandwidthMeter);
         HlsChunkSource textChunkSource = new HlsChunkSource(false /* isMaster */, textDataSource,
             url, manifest, DefaultHlsTrackSelector.newVttInstance(), bandwidthMeter,
             timestampAdjusterProvider, HlsChunkSource.ADAPTIVE_MODE_SPLICE);
@@ -176,5 +186,25 @@ public class HlsRendererBuilder implements RendererBuilder {
       renderers[DemoPlayer.TYPE_TEXT] = textRenderer;
       player.onRenderers(renderers, bandwidthMeter);
     }
+  }
+
+  private static UriDataSource createDataSource(Context context, String userAgent) {
+    return createDataSource(context, userAgent, null);
+  }
+
+  private static UriDataSource createDataSource(Context context, String userAgent,
+                                                TransferListener transferListener) {
+    return new DefaultUriDataSource(
+        context,
+        transferListener,
+        new OkHttpDataSource(getHttpClient(), userAgent, null, transferListener)
+    );
+  }
+
+  private static synchronized OkHttpClient getHttpClient() {
+    if (httpClient == null) {
+      httpClient = new OkHttpClient();
+    }
+    return httpClient;
   }
 }
