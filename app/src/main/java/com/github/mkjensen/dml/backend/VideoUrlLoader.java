da83f74679549e8dcd28608cb6bcefdb5c1d5bd7
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2016-03-26 17:36:56 +0100

    Add tests for VideoUrlLoader

--- app/src/main/java/com/github/mkjensen/dml/backend/VideoUrlLoader.java
+++ app/src/main/java/com/github/mkjensen/dml/backend/VideoUrlLoader.java
@@ -47,4 +47,9 @@ public class VideoUrlLoader extends BackendLoader<String> {
       return null;
     }
   }
+
+  @NonNull
+  String getLinksUrl() {
+    return linksUrl;
+  }
 }
---
+++ app/src/test/java/com/github/mkjensen/dml/backend/VideoUrlLoaderTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 Martin Kamp Jensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mkjensen.dml.backend;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.github.mkjensen.dml.test.RobolectricTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Tests for {@link VideoUrlLoader}.
+ */
+public class VideoUrlLoaderTest extends RobolectricTest {
+
+  @Rule
+  public final ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void constructor_whenCalledWithNullQuery_thenThrowsIllegalArgumentException() {
+
+    // When/then
+    thrown.expect(IllegalArgumentException.class);
+    @SuppressWarnings("ConstantConditions")
+    VideoUrlLoader loader = new VideoUrlLoader(getContext(), null);
+    assertNotNull(loader); // For your eyes only, PMD.
+  }
+
+  @Test
+  public void getLinksUrl_whenCalled_thenReturnsLinksUrlSuppliedToConstructor() {
+
+    // Given
+    String linksUrl = "test";
+    VideoUrlLoader loader = new VideoUrlLoader(getContext(), linksUrl);
+
+    // When
+    String actualLinksUrl = loader.getLinksUrl();
+
+    // Then
+    assertEquals(linksUrl, actualLinksUrl);
+  }
+}
